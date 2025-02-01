33c6813c229e687276acbd949a7178e65b5e3f5f
FabioMiranda-123 <fabio_rebelo@outlook.pt>
2025-02-01 14:07:51 +0000

    Solved Lab

--- tests/calculateArea.spec.js
+++ tests/calculateArea.spec.js
@@ -12,3 +12,31 @@ describe("Iteration 3", () => {
 })
 
 */
+describe("Iteration 3 | CalculateArea", () => {
+    describe("Function calculateArea", () => {
+      it("should be defined", () => {
+        expect(calculateArea).toBeDefined();
+      });
+  
+      it("should take two arguments", () => {
+        expect(calculateArea.length).toBe(2);
+      });
+  
+      it("should return the division of the two numbers", () => {
+        expect(calculateArea(6, 6)).toEqual(36);
+        expect(calculateArea(15, 15)).toEqual(225);
+        expect(calculateArea(20, 20)).toEqual(400);
+      });
+  
+      it("should return undefined if any of the arguments is not provided", () => {
+        expect(calculateArea(1)).toEqual(undefined);
+        expect(calculateArea()).toEqual(undefined);
+        expect(calculateArea(undefined, 1)).toEqual(undefined);
+      });
+      it ("should return undefined if any of the arguments is not not a number", () =>{
+        expect(calculateArea(1, "2")).toEqual(undefined);
+        expect(calculateArea("3", 4)).toEqual(undefined);
+        expect(calculateArea("100",  "47")).toEqual(undefined);
+      });
+    });
+  });
